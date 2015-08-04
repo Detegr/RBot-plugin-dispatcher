08aa54ce7fd58f92cb663b4f85b6999f3f9ab1a3
Antti Keränen <detegr@gmail.com>
2015-08-04 20:24:12 +0300

    Refactoring + connection wait on first startup

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use std::sync::Arc;
 
 const SOCKETS: [&'static str; 1] = ["../RBot/sockets/irc.quakenet.org"];
 const TYPE: usize = 0;
+const RETRIES: usize = 10;
 
 fn main() {
     // TODO: Read from a config file
@@ -24,58 +25,77 @@ fn main() {
         threads.push(thread::spawn(move || {
             let mut conn = match UnixStream::connect(socket) {
                 Ok(conn) => BufReader::new(conn),
-                Err(e) => panic!(e.to_string())
+                Err(e) => {
+                    println!("{}", e.to_string());
+                    match retry_connection(socket) {
+                        Some(conn) => conn,
+                        None => ::std::process::exit(1)
+                    }
+                }
             };
+            let mut line = String::new();
             loop {
-                let mut line = String::new();
-                let res = conn.read_line(&mut line);
-                match res {
-                    Ok(0) => {
+                if let Ok(bytes) = conn.read_line(&mut line) {
+                    if bytes == 0 {
                         break;
                     }
-                    Ok(_) => {
-                        let splitted: Vec<&str> = line.split_whitespace().collect();
-                        if splitted.len() == 0 {
-                            continue;
-                        }
-                        let linetype = splitted[TYPE];
-                        if let Some(plugin_list) = plgs.get(linetype) {
-                            for plugin in plugin_list {
-                                println!("Running plugin {}", plugin);
-                                let output = match Command::new(plugin).output() {
-                                    Ok(output) => output.stdout,
-                                    Err(e) => {
-                                        println!("{}", e.to_string());
-                                        continue;
-                                    }
-                                };
-                                match String::from_utf8(output) {
-                                    Ok(out) => {
-                                        println!("Output: {}", out.trim_right());
-                                        let s = conn.get_mut();
-                                        match s.write(out.trim_right().as_ref())
-                                                .and_then(|_| s.write(b"\r\n"))
-                                                .and_then(|_| s.flush()) {
-                                            Err(e) => println!("{}", e.to_string()),
-                                            _ => {},
-                                        }
-                                    }
-                                    Err(e) => {
-                                        println!("{}", e.to_string());
-                                    }
-                                }
+                    let splitted: Vec<&str> = line.split_whitespace().collect();
+                    if splitted.len() == 0 {
+                        continue;
+                    }
+                    let linetype = splitted[TYPE];
+                    if let Some(plugin_list) = plgs.get(linetype) {
+                        for plugin in plugin_list {
+                            match run_plugin(plugin)
+                                      .and_then(|out| send_plugin_reply(conn.get_mut(), &out)) {
+                                Err(e) => println!("{}", e),
+                                _ => {}
                             }
                         }
                     }
-                    Err(e) => {
-                        println!("{}", e.to_string());
-                        break;
-                    }
                 }
+                else {
+                    println!("Could not read the socket");
+                    break;
+                }
+                line.clear();
             }
         }));
     }
     for t in threads {
         let _ = t.join();
     }
+}
+
+fn retry_connection(socket: &str) -> Option<BufReader<UnixStream>> {
+    for _ in 0..RETRIES {
+        println!("Retrying connection...");
+        if let Ok(conn) = UnixStream::connect(socket) {
+            return Some(BufReader::new(conn));
+        }
+        else {
+            thread::sleep_ms(1000);
+        }
+    }
+    None
+}
+
+fn run_plugin(plugin: &str) -> Result<String, String> {
+    println!("Running plugin {}", plugin);
+    let output = match Command::new(plugin).output() {
+        Ok(output) => output.stdout,
+        Err(e) => {
+            return Err(e.to_string());
+        }
+    };
+    String::from_utf8(output).map_err(|e| e.to_string())
+}
+
+fn send_plugin_reply(s: &mut UnixStream, output: &str) -> Result<(),String> {
+    match s.write(output.trim_right().as_ref())
+        .and_then(|_| s.write(b"\r\n"))
+        .and_then(|_| s.flush()) {
+            Err(e) => Err(e.to_string()),
+            _ => Ok(()),
+        }
 }
